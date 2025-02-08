0538a90c497b4597e55811285094960f09bd1d12
Evgenii Viriasov <markine087@gmail.com>
2025-02-08 10:39:53 +0300

    usersrepo created

---
+++ usersrepo/src/main/java/com/example/usersrepo/UsersrepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersrepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersrepoApplication.class, args);
+    }
+
+}
---
+++ usersrepo/src/test/java/com/example/usersrepo/UsersrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersrepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
