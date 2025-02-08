d2d45239626c3ef5666de2fe741e2e88758be95c
Evgenii Viriasov <markine087@gmail.com>
2025-02-08 16:24:48 +0300

    create auth repo

---
+++ authrepo/src/main/java/com/example/authrepo/AuthrepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.authrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthrepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthrepoApplication.class, args);
+    }
+
+}
---
+++ authrepo/src/test/java/com/example/authrepo/AuthrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthrepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
