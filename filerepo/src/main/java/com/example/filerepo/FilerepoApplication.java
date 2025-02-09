863e234b148644ffbf473541b498db25fbba632c
Evgenii Viriasov <markine087@gmail.com>
2025-02-09 10:35:29 +0300

    create filerepo

---
+++ filerepo/src/main/java/com/example/filerepo/FilerepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.filerepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilerepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilerepoApplication.class, args);
+    }
+
+}
---
+++ filerepo/src/test/java/com/example/filerepo/FilerepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filerepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilerepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
